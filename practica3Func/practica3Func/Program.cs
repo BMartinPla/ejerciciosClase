b3cc5ea3946f68c67355bf1db9132d2721304cd4
Pla <braian.pla@FRSN.UTN.EDU.AR>
2025-08-01 20:01:17 -0300

    Clase 3

---
+++ practica3Func/practica3Func/Program.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace practica2Calc
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string cadena, operador;
+            int a, b;
+
+            Console.WriteLine("Ingrese el primer numero: ");
+            cadena = Console.ReadLine();
+            while (int.TryParse(cadena, out a) == false)
+            {
+                Console.WriteLine("Ingrese el primer numero: ");
+                cadena = Console.ReadLine();
+            }
+
+            Console.WriteLine("Ingrese el segundo numero: ");
+            cadena = Console.ReadLine();
+            while (int.TryParse(cadena, out b) == false)
+            {
+                Console.WriteLine("Ingrese el segundo numero: ");
+                cadena = Console.ReadLine();
+            }
+
+            Console.WriteLine("Ingrese el operador (/-*+)");
+            operador = Console.ReadLine();
+            while (operador != "/" && operador != "-" && operador != "*" && operador != "/")
+            {
+                Console.WriteLine("Ingrese el operador (/-*+)");
+                operador = Console.ReadLine();
+            }
+
+            switch (operador)
+            {
+                case "+":
+                    {
+                        Console.WriteLine($"El resultado es: {sum(a, b)}");
+                        break;
+                    }
+                case "-":
+                    {
+                        Console.WriteLine($"El resultado es: {res(a, b)}");
+                        break;
+                    }
+                case "/":
+                    {
+                        try
+                        {
+                            Console.WriteLine($"El resultado es: {div (a, b)}");
+                        }
+                        catch (Exception ex)
+                        {
+                            Console.WriteLine(ex.Message);
+                        }
+                        finally
+                        {
+                            Console.WriteLine("Operacion finalizada.");
+                        }
+                        break;
+                    }
+                case "*":
+                    {
+                        Console.WriteLine($"El resultado es: {mul(a, b)}");
+                        break;
+                    }
+                default:
+                    {
+                        Console.WriteLine("El operador no es valido.");
+                        break;
+                    }
+            }
+
+            Console.ReadKey();
+        }
+
+        static int sum(int a, int b)
+        {
+            return a + b;
+        }
+
+        static int res(int a, int b)
+        {
+            return a - b;
+        }
+
+        static int div(int a, int b)
+        {
+            if (b != 0)
+            {
+                return a / b;
+            }
+            else
+            {
+                Exception e = new Exception("Imposible dividir por cero.");
+                e.Source = "Procedimiento de division";
+                e.HelpLink = "Leer fundamentos matematicos";
+                throw e;
+            }
+        }
+
+        static int mul(int a, int b)
+        {
+            return a * b;
+        }
+    }
+}
