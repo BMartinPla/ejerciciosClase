4cbd1047dea23c5aa6dfe47b95a1deb5660fa8d7
Pla <braian.pla@FRSN.UTN.EDU.AR>
2025-08-22 21:17:15 -0300

    cambio

---
+++ practica16CUIT/practica16CUIT/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace practica16CUIT
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string masc = "20";
+            string fem = "27";
+            string ind = "24";
+            string emp = "30";
+            string dni;
+            int tdni;
+            int aux;
+
+            Console.WriteLine("Tipo de DNI");
+            Console.WriteLine("1 - Masculino");
+            Console.WriteLine("2 - Femenino");
+            Console.WriteLine("3 - Indeterminado");
+            Console.WriteLine("4 - Empresa");
+            tdni = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Ingrese su DNI");
+            dni = Console.ReadLine();
+
+            string dnicomp = "";
+
+            switch(tdni)
+            {
+                case 1:
+                    dnicomp = masc + dni;
+                    break;
+                case 2:
+                    dnicomp = fem + dni;
+                    break;
+                case 3:
+                    dnicomp = ind + dni;
+                    break;
+                case 4:
+                    dnicomp = emp + dni;
+                    break;
+            }
+
+            aux = int.Parse(dnicomp[0].ToString()) * 5;
+            aux = aux + int.Parse(dnicomp[1].ToString()) * 4;
+
+            aux = aux + int.Parse(dnicomp[2].ToString()) * 3;
+            aux = aux + int.Parse(dnicomp[3].ToString()) * 2;
+            aux = aux + int.Parse(dnicomp[4].ToString()) * 7;
+            aux = aux + int.Parse(dnicomp[5].ToString()) * 6;
+            aux = aux + int.Parse(dnicomp[6].ToString()) * 5;
+            aux = aux + int.Parse(dnicomp[7].ToString()) * 4;
+            aux = aux + int.Parse(dnicomp[8].ToString()) * 3;
+            aux = aux + int.Parse(dnicomp[9].ToString()) * 2;
+
+            int aux2 = aux;
+            aux = aux / 11;
+            aux2 = aux2 - (aux * 11);
+            aux2 = 11 - aux2;
+
+            Console.WriteLine(aux2);
+            Console.ReadKey();
+
+        } 
+    }
+}
---
+++ practica16CUIT/practica16CUIT/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// La información general de un ensamblado se controla mediante el siguiente 
+// conjunto de atributos. Cambie estos valores de atributo para modificar la información
+// asociada a un ensamblado.
+[assembly: AssemblyTitle("practica16CUIT")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("practica16CUIT")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Si establece ComVisible en false, los tipos de este ensamblado no estarán visibles 
+// para los componentes COM.  Si es necesario obtener acceso a un tipo en este ensamblado desde 
+// COM, establezca el atributo ComVisible en true en este tipo.
+[assembly: ComVisible(false)]
+
+// El siguiente GUID sirve como id. de typelib si este proyecto se expone a COM.
+[assembly: Guid("f841d705-8e90-46cd-ac06-8607b347ccfe")]
+
+// La información de versión de un ensamblado consta de los cuatro valores siguientes:
+//
+//      Versión principal
+//      Versión secundaria
+//      Número de compilación
+//      Revisión
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
