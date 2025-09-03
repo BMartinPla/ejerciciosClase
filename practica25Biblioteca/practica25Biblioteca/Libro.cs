6e66ed6b0686c69fb02f6b8831f5592ab9430238
Pla <braian.pla@FRSN.UTN.EDU.AR>
2025-09-03 20:49:21 -0300

    cambio

---
+++ practica25Biblioteca/practica25Biblioteca/Libro.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace practica25Biblioteca
+{
+    public enum tapa
+    {
+        Blanda,
+        Intermedia,
+        Dura
+    }
+    public class Libro
+    {
+        private string _Titulo;
+        private string _Autor;
+        private bool _Disponible;
+        private tapa _TipoTapa;
+
+        public Libro (string Titulo, string Autor, tapa TipoTapa)
+        {
+            this.Titulo = Titulo;
+            this.Autor = Autor;
+            this.TipoTapa = TipoTapa;
+            this.Disponible = true;
+        }
+
+
+
+        public string Titulo
+        {
+            get { return this._Titulo; }
+            set { this._Titulo = value; }
+        }
+
+        public string Autor
+        {
+            get { return this._Autor; }
+            set { this._Autor = value; }
+        }
+
+        public bool Disponible
+        {
+            get { return this._Disponible; }
+            set { this._Disponible = value; }
+        }
+
+        public tapa TipoTapa
+        {
+            get { return this._TipoTapa}
+            set { this._TipoTapa = value; }
+        }
+
+
+    }
+}
---
+++ practica25Biblioteca/practica25Biblioteca/Usuario.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace practica25Biblioteca
+{
+    public class Usuario
+    {
+        private string _Nombre;
+        private string[] _LibrosPrestados;
+
+
+        public string Nombre
+        {
+            get { return this._Nombre; }
+            set { this._Nombre = value; }
+        }
+
+        public string[] LibrosPrestados
+        {
+            get { return this.LibrosPrestados; }
+            set { this.LibrosPrestados = value; }
+        }
+
+        MostrarLibrosPrestados()
+        {
+            for (int i = 0; i < LibrosPrestados.Length; i++)
+            {
+                Console.WriteLine(LibrosPrestados[i]);
+            }
+        }
+
+
+    }
+}
