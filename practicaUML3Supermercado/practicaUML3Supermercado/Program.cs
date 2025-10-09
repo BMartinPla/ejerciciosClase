42535498caddf15f45e519c3ac800df7af281e04
Pla <braian.pla@FRSN.UTN.EDU.AR>
2025-10-08 21:53:28 -0300

    cambio

---
+++ practicaUML3Supermercado/practicaUML3Supermercado/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.Json;
+using System.IO;
+
+namespace practicaUML3Supermercado
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            List<Supermercado> Cadena = new List<Supermercado>();
+
+            string Aux = File.ReadAllText("C:\\Users\\braian.pla\\source\\repos\\practicaUML3Supermercado\\practicaUML3Supermercado\\Datos.json");
+
+            Cadena = JsonSerializer.Deserialize<List<Supermercado>>(Aux);
+
+            /*
+            Supermercado CadAux = new Supermercado();
+ 
+            CadAux = JsonSerializer.Deserialize<Supermercado>(Aux);
+            */
+
+
+            foreach (Supermercado Sup in Cadena)
+            {
+                Console.WriteLine($"{Sup.nombre} - {Sup.direccion}");
+
+                foreach (Producto p in Sup.productos)
+                {
+                    Console.WriteLine($"Producto: {p.descripcion}");
+                }
+
+                Console.WriteLine();
+            }
+
+
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
---
+++ practicaUML3Supermercado/practicaUML3Supermercado/Supermercados.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace practicaUML3Supermercado
+{
+
+    public class Supermercados
+    {
+        public Supermercado[] Property1 { get; set; }
+    }
+
+    public class Supermercado
+    {
+        public string nombre { get; set; }
+        public string direccion { get; set; }
+        public Producto[] productos { get; set; }
+    }
+
+    public class Producto
+    {
+        public int id { get; set; }
+        public string descripcion { get; set; }
+        public double precioCosto { get; set; }
+        public double porcentajeGanancia { get; set; }
+        public double porcentajeDescuento { get; set; }
+        public double iva { get; set; }
+        public Ubicacion ubicacion { get; set; }
+        public double precioBruto { get { return this.precioCosto + ((this.precioCosto * this.porcentajeGanancia)/100); } }
+        public double montoDescuento { get { return (this.precioBruto * this.porcentajeDescuento)/100; } }
+        public double precioFinal { get { return (this.precioBruto - this.montoDescuento) + (((this.precioBruto - this.montoDescuento) * this.iva)/100); } }
+    }
+
+    public class Ubicacion
+    {
+        public string pasillo { get; set; }
+        public string estante { get; set; }
+    }
+
+}
