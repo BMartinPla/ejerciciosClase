32cede2a32fa88f32fa858cd7d0f16a0df105260
Pla <braian.pla@FRSN.UTN.EDU.AR>
2025-10-13 19:55:34 -0300

    cambio

---
+++ practicaUML4http/practicaUML4http/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.Json;
+using System.Net.Http;
+
+namespace practicaUML4http
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+
+            try
+            {
+                HttpClient client = new HttpClient();
+
+
+                F1 F1Datos = new F1();
+
+                /*
+                string temp = string.Empty;
+                string race = string.Empty;
+
+                Console.Write("Ingrese el año: ");
+                temp = Console.ReadLine();
+
+                Console.Write("Ingrese la carrera: ");
+                race = Console.ReadLine();
+                */
+
+                string cad;
+                int temp;
+                int race;
+
+                Console.Write("Ingrese el año: ");
+                cad = Console.ReadLine();
+                while (!int.TryParse(cad, out temp))
+                {
+                    Console.Write("Ingrese el año: ");
+                    cad = Console.ReadLine();
+                }
+
+                Console.Write("Ingrese la carrera: ");
+                cad = Console.ReadLine();
+                while (!int.TryParse(cad, out race))
+                {
+                    Console.Write("Ingrese la carrera: ");
+                    cad = Console.ReadLine();
+                }
+
+
+                string urlApi = $"https://api.jolpi.ca/ergast/f1/{temp}/{race}/results/";
+                string datos = await client.GetStringAsync(urlApi);
+
+                F1Datos = JsonSerializer.Deserialize<F1>(datos);
+
+                Console.Clear();
+
+                Console.WriteLine($"TEMPORADA {temp} | CARRERA N°{race} | {F1Datos.MRData.RaceTable.Races[0].Circuit.circuitName}");
+                foreach (Result Res in F1Datos.MRData.RaceTable.Races[0].Results)
+                {
+                    Console.WriteLine($"P{Res.position}. {Res.Driver.familyName}, {Res.Driver.givenName} - {Res.Constructor.name} | {Res.Time?.time}");
+                }
+
+
+                Console.ReadKey();
+            }
+
+            catch
+            {
+
+            }
+        }
+    }
+}
---
+++ practicaUML4http/practicaUML4http/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// La información general de un ensamblado se controla mediante el siguiente 
+// conjunto de atributos. Cambie estos valores de atributo para modificar la información
+// asociada a un ensamblado.
+[assembly: AssemblyTitle("practicaUML4http")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("practicaUML4http")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Si establece ComVisible en false, los tipos de este ensamblado no estarán visibles 
+// para los componentes COM.  Si es necesario obtener acceso a un tipo en este ensamblado desde 
+// COM, establezca el atributo ComVisible en true en este tipo.
+[assembly: ComVisible(false)]
+
+// El siguiente GUID sirve como id. de typelib si este proyecto se expone a COM.
+[assembly: Guid("626d728a-34c4-4d46-8030-a1c7c7685026")]
+
+// La información de versión de un ensamblado consta de los cuatro valores siguientes:
+//
+//      Versión principal
+//      Versión secundaria
+//      Número de compilación
+//      Revisión
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
